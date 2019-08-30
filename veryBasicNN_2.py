cf32e4aee1ac402596bed9b4b79436e2cd9ff8cd
Viona91 <vionaquadros@gmail.com>
2019-08-31 00:51:17 +0530

    Update veryBasicNN_2.py

--- veryBasicNN_2.py
+++ veryBasicNN_2.py
@@ -12,133 +12,191 @@ def sigmoid(z):
    '''
    return (1/(1 + np.power(np.exp(z),(-1))))
    
-def end_loss(y, pred_y):
+def initialize_parameters(n_x, network_dimensions):
    '''
-   Difference between predicted value and actual value
+   Function to initialize parameters
+
+   Arguments:
+   n_x -- dimension of input
+   network_dimensions -- dimension of hidden layers and output (total L)
+
+   Returns:
+   parameters -- initialized parameters W1, b1, ... WL, bL
+   '''
+   parameters = {}
+   parameters['W1'] = np.random.randn(network_dimensions[0], n_x.shape[0]) * 0.01
+   parameters['b1'] = np.zeros((network_dimensions[0], 1))
+   for l in range(len(network_dimensions)-1):
+      parameters['W' + str(l+2)] = np.random.randn(network_dimensions[l+1], network_dimensions[l]) * 0.01
+      parameters['b' + str(l+2)] = np.zeros((network_dimensions[l+1], 1))
+   return parameters
+
+def forward_propogation(x, parameters, L):
+   '''
+   Function to implement forward propogation of neural network
    
    Arguments:
-   y -- actual value
-   pred_y -- predicted value
+   x -- initial input for neural network
+   parameters -- initialized parameters for neural network (using initialize_parameters function)
+   activations -- cache for storing activations value
+   L -- number of layers of neural network
    
    Returns:
-   difference between y and pred_y
+   AL -- final value determined from forward propogation
+   activations -- caching weigths W, biases b and intermediate values Z (linear activation) for backward propogation
    '''
-   return np.subtract(y, pred_y)
+   activations = {}
+   activations['A0'] = x
+   prev_A = x
+   #ReLU activation for L-1 layers
+   for l in range(1, L):
+      activations['Z' + str(l)] = np.dot(parameters['W' + str(l)], prev_A) + parameters['b' + str(l)]
+      activations['A' + str(l)] = np.maximum(np.zeros(activations['Z' + str(l)].shape), activations['Z' + str(l)]) #ReLU activation
+      prev_A = activations['A' + str(l)]
+     
+   #sigmoid activation for last layer (binary classification
+   activations['Z' + str(L)] = np.dot(parameters['W' + str(L)], prev_A) + parameters['b' + str(L)]
+   activations['A' + str(L)] = sigmoid(activations['Z' + str(L)]) #ReLU activation
+   return activations['A' + str(L)], activations
    
-def initialize_random(m,n):
+def cost_function(AL, Y):
    '''
-   Initialze random weights according to dimension provided
+   Cost function for sigmoid activation function - J = -(1/m)(Y log(AL) + (1-Y) log(1-AL))
    
    Arguments:
-   m -- row dimension
-   n -- column dimension
+   AL -- output from forward propogation
+   Y -- actual output of training data
    
    Returns:
-   randomly initialized matrix of dimension m*n
+   J -- value of the cost function
    '''
-   return np.random.rand(m, n)
+   m = Y.shape[1]
+   J = -(1/m) * (np.sum(np.multiply(Y, np.log(AL)) - np.multiply((1-Y), np.log(1-AL))))
+   return J
    
-def predict(y):
+def relu_backward(dA, z):
    '''
-   Sigmoid value >= 0.5 is 1 else 0
+   Compute dZ. Gradient of ReLU is 0 if value is <= 0 else value. dZ = dA * g'(z)
    
    Arguments:
-   y -- value of sigmoid activation function
+   dA -- The base value to be considered
+   z -- value to be queried
    
    Returns:
-   Truth value 0 or 1
+   dZ -- The ReLU backward value
    '''
-   return (1 if y > 0.5 else 0)
+   dZ = np.array(dA, copy = True) # A and Z are same. So only need to copy and filter for 0
+   dZ[z <= 0] = 0 #set value to 0 if z is 0
+   return dZ
    
-def calculate_delta(W, next_delta, z):
+def sigmoid_backward(dA, z):
    '''
-   Calculate the error value (delta) of a layer
+   Compute dZ. Gradient of sigmoid. dZ = dA * g'(z) = dA * s * (1-s) where s is the sigmoid of Z
    
    Arguments:
-   W -- weight matrix of to be updated of current layer
-   next_delta -- value of next layer delta
-   z -- value of weightmatrix to activation function of current layer
+   dA -- The base value to be considered
+   z -- value to be queried
    
    Returns:
-   delta -- error value of current layer
+   dZ -- The sigmoid backward value
    '''
-   delta = np.multiply(np.dot(W, next_delta), np.multiply(z, np.subtract(1,z)))
-   return delta
+   s = sigmoid(z)
+   dZ = dA * s * (1-s)
+   return dZ
    
-def backpropogate(W, delta, a):
+def backward_propogation(Y, AL, parameters, activations, L):
    '''
-   Update weights according to delta of next layer and activation function of current layer
+   Function to implement backward propogation of neural network
    
    Arguments:
-   W -- weight matrix
-   delta -- error of next layer
-   a -- activation of current layer
+   Y -- actual output for neural network
+   AL -- output predicted from feedforward pass
+   parameters -- initialized parameters for neural network (using initialize_parameters function)
+   activations -- activations from feedforward pass
+   L -- number of layers of neural network
    
    Returns:
-   Updated weight matrix
+   cache -- caching weigths dW, biases db for updating of parameters
    '''
-   return np.add(W, np.dot(a, delta.T))
+   m = Y.shape[1]
+   cache = {}
+   cache['dA' + str(L)] = -np.divide(Y, AL) + np.divide((1-Y), (1-AL))
+   cache['dZ' + str(L)] = sigmoid_backward(cache['dA' + str(L)], activations['Z' + str(L)])
+   cache['dW' + str(L)] = (1/m) * np.dot(cache['dZ' + str(L)], activations['A' + str(L-1)].T)   
+   cache['db' + str(L)] = (1/m) * np.sum(cache['dZ' + str(L)], axis=1, keepdims=True)
+   cache['dA' + str(L-1)] = np.dot(parameters['W' + str(L)].T, cache['dZ' + str(L)])
    
-def weights_by_features(W, x):
+   for l in range(L-1, 0, -1):
+      cache['dZ' + str(l)] = relu_backward(cache['dA' + str(l)], activations['Z' + str(l)])
+      cache['dW' + str(l)] = (1/m) * np.dot(cache['dZ' + str(l)], activations['A' + str(l-1)].T)   
+      cache['db' + str(l)] = (1/m) * np.sum(cache['dZ' + str(l)], axis=1, keepdims=True)
+      cache['dA' + str(l-1)] = np.dot(parameters['W' + str(l)].T, cache['dZ' + str(l)])
+   return cache 
+
+def update_parameters(learning_rate, parameters, cache, L):
    '''
-   The weights by features calculation function
+   Update parameters with values got from backward propogation
    
    Arguments:
-   x -- input vector of dimension 3*1
-   W -- weight matrix
+   learning_rate -- alpha. update with the multiple of learning rate
+   parameters -- parameters for neural network (weights and biases)
+   cache -- weigths dW, biases db from backward propogation
+   L -- number of layers of neural network
    
    Returns:
-   (W.T * x) -- vector W + vector x
+   parameters -- updated parameters for neural network (weights and biases)
    '''
-   return np.dot(W.T, x)
+   for l in range(1, L+1):
+      parameters['W' + str(l)] -= (learning_rate * cache['dW' + str(l)])
+      parameters['b' + str(l)] -= (learning_rate * cache['db' + str(l)])
+   return parameters
    
-def networkRun(x, y, W1, W2):
-   '''
-   Run the feedforward and backpropogation of a single layer NN to learn outputs
+def n_layer_neural_network(x, y, network_dimensions, learning_rate):
+   L = len(network_dimensions)
+   parameters = initialize_parameters(x, network_dimensions)
+   for i in range(1500):
+      AL, activations = forward_propogation(x, parameters, L)
+      J = cost_function(AL, y)
+      #print (J)
+      cache = backward_propogation(y, AL, parameters, activations, L)
+      parameters = update_parameters(learning_rate, parameters, cache, L)
+   print ("Cost: " + str(J))
+   return parameters
    
-   Arguments:
-   x -- input vector with m examples of dimension n in matrix form m*n
-   y -- corresponding output vector of order m*1
-   W1 -- randomly initialized weight matrix of order n * ?
-   W2 -- randomly initialized weight matrix of oredr ? * 1
-   '''
-   
-   #number of iterations
-   for count in range(200):
-      #print("Iteration: " + str(count))
-      input = np.random.choice(np.arange(len(x))) #randomly choose input for training
-      x_input = np.concatenate([1], x[input])
-      print (x_input)
-      print (x[input])
-      print (len(x[input]))
-      break 
-      '''
-      activation_layer1 = np.asmatrix(x[input]).T # add extra variables for vector multiplcation with bias; shape 3*1
-      z2 = np.asmatrix(weights_by_features(W1, x[input])).T #shape 6*1
-      #print ("z2: " + str(z2.shape))
-      activation_layer2 = sigmoid(z2) #shape 6*1
-      #print ("activation_layer2: " + str(activation_layer2.shape))
-      z3 = weights_by_features(W2, activation_layer2) #shape 1*1
-      #print ("z3: " + str(z3.shape))
-      activation_layer3 = sigmoid(z3) #shape 1*1
-      #print ("activation_layer3: " + str(activation_layer3.shape))
-      pred_y = predict(activation_layer3) #output shape 1*1
-      #print ("pred_y: " + str(np.asmatrix(pred_y).shape))
-      print ("X: " + str(x[input]) + " Y: " + str(pred_y) + " Actual Y: " + str(y[input]))
-      #run backpropogation to update weights
-      delta_3 = end_loss(y[input], pred_y)
-      delta_2 = calculate_delta(W2, delta_3, activation_layer2) #shape 6*1
-      #print ("delta_2: " + str(delta_2.shape))
-      W2 = backpropogate(W2, delta_3, activation_layer2) #shape 6*1
-      #print ("W2: " + str(W2.shape))
-      delta_1 = calculate_delta(W1, delta_2, activation_layer1) #shape 1*1
-      #print ("delta_1: " + str(delta_1.shape))
-      W1 = backpropogate(W1, delta_2, activation_layer1) #shape 3*6
-      #print ("W1: " + str(W1.shape))
-      '''
+def predict_y(predicted_output):
+	return (predicted_output <= 0.5)
+
 #Learn the network by providing inputs and outputs of XOR gate
-x = np.array([[1,1],[0,0],[0,1],[1,0]])
-y = np.array([0,0,1,1])
-W1 = initialize_random(2, 6)
-W2 = initialize_random(6, 1)
-networkRun(x, y, W1, W2)
+x = np.array([[1,1],[0,0],[0,1],[1,0]]).T #shape (2, 4)
+y = np.reshape([0,0,1,1], (1,4)) #shape (1, 4)
+network_dimensions = [6, 4, 2, 1]
+learning_rate = 0.01
+parameters = n_layer_neural_network(x, y, network_dimensions, learning_rate)
+#predict output
+predicted_output, _ = forward_propogation(x, parameters, L = len(network_dimensions))
+nn_y = predict_y(predicted_output)
+for i in range(x.shape[1]):
+	print (str(x[0][i]) + " " + str(x[1][i]) + " : " + str(nn_y[0][i]))
+
+'''
+parameters = initialize_parameters(x, network_dimensions)
+AL, activations = forward_propogation(x, parameters, L)
+J = cost_function(AL, y)
+cache = backward_propogation(y, AL, parameters, activations, L)
+parameters = update_parameters(learning_rate, parameters, cache, L)
+
+#print shape of the initialized parameters
+for key in parameters:
+   print (key + " : " + str(parameters[key].shape))
+
+#print shape of the activation parameters   
+for key in cache:
+   print (key + " : " + str(cache[key].shape))
+
+#testing for ReLU activation
+print (activations['Z1'])
+print (np.maximum(np.zeros(activations['Z1'].shape), activations['Z1']))
+
+#cost
+print (J)
+'''
